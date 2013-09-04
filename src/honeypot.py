f153257dbca7f03ad15414ae9610e41412d1bd6d
katkad <durechova@ynet.sk>
2013-09-04 14:01:03 -0400

    Add hpfeeds to honeypot
    + add hpfeeds variables to config
    + bind ip should be specified, instead "0.0.0.0" is published

--- src/honeypot.py
+++ src/honeypot.py
@@ -15,7 +15,11 @@ import SocketServer
 import os
 import random
 import datetime
-# import artillery modules
+import json
+import logging
+logging.basicConfig()
+import hpfeeds
+
 from src.core import *
 from src.smtp import *
 
@@ -24,6 +28,19 @@ ports = check_config("PORTS=")
 # check to see what IP we need to bind to
 bind_interface = check_config("BIND_INTERFACE=")
 send_email = check_config("ALERT_USER_EMAIL=")
+
+hpfeeds_host = check_config("HPFEEDS_HOST=")
+if hpfeeds_host:
+    hpc = hpfeeds.new(
+        hpfeeds_host,
+        int(check_config("HPFEEDS_PORT=")),
+        check_config("HPFEEDS_IDENT="),
+        check_config("HPFEEDS_SECRET=")
+    )
+    write_log("Connected to {0}".format(hpc.brokername))
+
+    hpf_channels_string = check_config("HPFEEDS_CHANNELS=")
+    hpf_channels = hpf_channels_string[1:-1].split(',')
 
 # main socket server listener for responses
 class SocketListener((SocketServer.BaseRequestHandler)):
@@ -50,6 +67,15 @@ class SocketListener((SocketServer.BaseRequestHandler)):
                     log_message = "%s [!] Artillery has detected an attack from IP address: %s for a connection on a honeypot port: %s" % (now,self.client_address[0],self.server.server_address[1])
 
                     write_log(log_message)
+
+                    if hpfeeds_host:
+                        data = {
+                            "local_host": self.server.server_address[0],
+                            "remote_port": self.client_address[1],
+                            "local_port": self.server.server_address[1],
+                            "remote_host": self.client_address[0]
+                        }
+                        hpc.publish(hpf_channels, json.dumps(data))
 
                     email_alerts = check_config("EMAIL_ALERTS=").lower()
 
